fa8865e7ad33fb89d5e2f48913f78a23d14cc228
anirban2005143a <23je0104@iitism.ac.in>
2025-05-19 22:31:54 +0530

    fix hright issue with chat window

--- frontend/src/components/chatting_window/ChatInterface.jsx
+++ frontend/src/components/chatting_window/ChatInterface.jsx
@@ -12,7 +12,7 @@ const ChatInterface = () => {
         <Navbar />
 
         {/* chat window  */}
-        <div className="workSpace pt-[55px] h-screen ">
+        <div className="workSpace pt-[55px] h-full ">
           <ChatWindow />
         </div>
       </div>
