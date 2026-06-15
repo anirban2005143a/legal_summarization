7212413e6a3b785edec4ca534d16284b416e66cb
Divyanshu <divyanshuchoubey8210@gmail.com>
2026-06-15 15:07:27 +0530

    deployment update

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-     webpack: (config) => {
+  env: {
+    NEXT_PUBLIC_FAST_URL: "https://divyanshu8210-legalsummary.hf.space",
+  },
+  webpack: (config) => {
     // Ignore PDF files inside node_modules during build
     config.module.rules.push({
       test: /\.pdf$/,
